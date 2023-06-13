f4e2e97b76e47563320d58f9f86eb09adbfab345
Иван Матящук <imatyashuk@gmail.com>
2023-06-13 12:37:11 +0300

    Сделал 2 функции

---
+++ js/function.js
@@ -0,0 +1,35 @@
+// Функция для проверки длины строки.
+const detectStringLength = (string, maxLength) => {
+  if(string.length <= maxLength) {
+    console.log(true);
+    return true;
+  }
+  console.log(false);
+  return false;
+};
+
+detectStringLength('wwww',5);
+
+//Функция для проверки, является ли строка палиндромом.
+
+const detectStringPalindrom = (string) => {
+  const newString = string.replaceAll(' ', '');
+  newString.toLowerCase();
+  let reverseString = '';
+  console.log(newString.toLowerCase());
+
+  for(let i = newString.length - 1; i >= 0; i = i - 1) {
+    reverseString += newString.at(i);
+    reverseString.toLowerCase();
+    console.log(reverseString);
+  }
+
+  if(reverseString === newString) {
+    console.log('Это палиндром');
+    return 'Это палиндром';
+  }
+  console.log('Это не палиндром');
+  return 'Это не палиндром';
+};
+
+detectStringPalindrom('Шалаш');
