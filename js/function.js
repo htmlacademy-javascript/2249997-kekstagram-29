1263213c9a42c55bd76b67fd8d0cce94e089ac44
Иван Матящук <imatyashuk@gmail.com>
2023-06-19 21:47:39 +0300

    поправил функции из прошлого задания

--- js/function.js
+++ js/function.js
@@ -1,10 +1,9 @@
+/* eslint-disable no-console */
 // Функция для проверки длины строки.
 const detectStringLength = (string, maxLength) => {
   if(string.length <= maxLength) {
-    console.log(true);
     return true;
   }
-  console.log(false);
   return false;
 };
 
@@ -15,19 +14,17 @@ detectStringLength('wwww',5);
 const detectStringPalindrom = (string) => {
   const newString = string.replaceAll(' ', '').toLowerCase();
   let reverseString = '';
-  console.log(newString.toLowerCase());
 
   for(let i = newString.length - 1; i >= 0; i = i - 1) {
     reverseString += newString.at(i).toLowerCase();
-    console.log(reverseString);
   }
 
   if(reverseString === newString) {
-    console.log('Это палиндром');
     return 'Это палиндром';
   }
-  console.log('Это не палиндром');
   return 'Это не палиндром';
 };
 
 detectStringPalindrom('ШалаШ');
+detectStringPalindrom('Леша на полке клопа нашел');
+detectStringPalindrom('ТАНЯ');
