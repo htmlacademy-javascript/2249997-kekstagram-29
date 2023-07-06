b8c0192ba4cc56610528739142de718b43f365fc
Иван Матящук <imatyashuk@gmail.com>
2023-07-07 00:07:43 +0300

    сделал задание 8.1 Открывается закрывается

--- js/full-screen-rendering.js
+++ js/full-screen-rendering.js
@@ -14,23 +14,23 @@ const onDocumentKeyDown = (evt) => {
 
 // Функция открытия полноразмерного изображения
 function openFullImage () {
-  fullImage.classList.remove('.hidden');
+  fullImage.classList.remove('hidden');
 
   document.addEventListener('keydown', onDocumentKeyDown);
 
   // Навесил класс чтобы контейнер с фотографиями позади не прокручивался при скролле
-  document.querySelector('body').classList.add('.modal-open');
+  document.querySelector('body').classList.add('modal-open');
 
   // спрячьте блоки счётчика комментариев
-  document.querySelector('.social__comment-count').classList.add('.hidden');
+  document.querySelector('.social__comment-count').classList.add('hidden');
 
   // спрячьте блоки загрузки новых комментариев
-  document.querySelector('.comments-loader').classList.add('.hidden');
+  document.querySelector('.comments-loader').classList.add('hidden');
 }
 
 // Функция закрытия полноразмерного изображения
 function closeFullImage () {
-  fullImage.classList.add('.hidden');
+  fullImage.classList.add('hidden');
 
   document.removeEventListener('keydown', onDocumentKeyDown);
 
